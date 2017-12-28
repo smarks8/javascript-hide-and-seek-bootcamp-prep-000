006432cf709ee4096926480e6c96874078b04bd5
stacey <staceyamarks@gmail.com>
2017-12-28 17:43:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('div.target')
+}
+
+function increaseRankBy(n){
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  
+  for(var i = 0; i< list.length;i++){
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  var list = document.getElementById('grand-node').querySelectorAll('div')
+  
+  var last = list[list.length-1]
+  
+  return last
+  
+}
+
